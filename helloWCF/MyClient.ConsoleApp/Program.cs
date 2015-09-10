a4448c51701b2eb30b6ec6dbce1f214588e4832f
Shaun Luttin <shaunluttin@gmail.com>
2015-09-10 12:10:37 -0700

    Create a Wcf service, two hosts, and a client.

---
+++ helloWCF/MyClient.ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClient.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new ServiceReference1.Service1Client();
+            var result = client.GetData(5);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ helloWCF/ServiceHost.ConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+namespace MySelfHost.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var host = new ServiceHost(typeof(WcfServiceLibrary1.Service1)))
+            {
+                host.Open();
+
+                Console.WriteLine("The service is ready.");
+                Console.WriteLine("Press <Enter> to stop the service.");
+                Console.ReadLine();
+
+                // Close the ServiceHost.
+                host.Close();
+            }
+        }
+    }
+}
